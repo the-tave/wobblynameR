e1ab46643f7a47d5afd161dbb707c8894675dea2
the-tave <120721022+the-tave@users.noreply.github.com>
2022-12-16 11:59:42 +0100

    Add files via upload
    to setup version 0.0.9 of wobblenameR (including only namepref as function)

---
+++ R/namepref.R
@@ -0,0 +1,41 @@
+#' Add name prefix
+#'
+#' This function strips the old prefix from the variable names of dataframe.
+#' It then adds the new prefix where the old one was and changes the dataframe's names to the new ones.
+#'
+#' @param dataframe A dataframe
+#' @param pref_old Old prefix character that some or all variables in the dataframe have
+#' @param pref_new New prefix character that shall replace the old one
+#'
+#' @return Returns dataframe with those variables renamed, that start wwith the old prefix.
+#' @export
+#'
+#' @examples
+#' bla <- tibble::tibble(x_ar = 1:5, y_ar = 6:10)
+#' blo <- namepref(bla, "x_", "z_")
+#'
+#' names(bla)
+#' names(blo)
+#'
+namepref <- function(dataframe, pref_old, pref_new){
+  # This function strips the old prefix from the variable names of dataframe. It then adds the new prefix where the old one was
+  # and changes the dataframe's names to the new ones.
+
+  # pref_old and pref_new must be character strings, dataframe must be a dataframe
+
+  # namedf <- tibble::as_tibble(names(dataframe))  %>%
+  #   dplyr::mutate(stripped = ifelse(startsWith(value, pref_old), substr(value, (nchar(pref_old)+1), nchar(value)), value),
+  #                 # stripped = ifelse(startsWith(value, pref_old), substr(value, 3, nchar(value)), value),
+  #                 new_names = ifelse(startsWith(value, pref_old), paste0(pref_new, stripped), value))
+  #
+  # names(dataframe) <- namedf %>% dplyr::pull(new_names)
+
+  namedf <- tibble::as_tibble(names(dataframe))  |>
+    dplyr::mutate(stripped = ifelse(startsWith(value, pref_old), substr(value, (nchar(pref_old)+1), nchar(value)), value),
+                  # stripped = ifelse(startsWith(value, pref_old), substr(value, 3, nchar(value)), value),
+                  new_names = ifelse(startsWith(value, pref_old), paste0(pref_new, stripped), value))
+
+  names(dataframe) <- namedf |> dplyr::pull(new_names)
+
+  return(dataframe)
+}
