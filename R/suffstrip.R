41ae4237623d5a0d81fe6050c4e3c101a630ffd8
Tave <annika_tave.overlander@tu-dresden.de>
2022-12-16 16:33:22 +0100

    fixed suffstrip documentation

--- R/suffstrip.R
+++ R/suffstrip.R
@@ -9,7 +9,7 @@
 #'
 #' @examples
 #' bla <- tibble::tibble(ar_x = 1:5, ar_y = 6:10)
-#' blo <- namesuff(bla, "_x")
+#' blo <- suffstrip(bla, "_x")
 #' names(bla)
 #' names(blo)
 #' 
@@ -17,10 +17,12 @@ suffstrip <- function(dataframe, suf_old){
   # This function strips the old suffix and changes the dataframe's names to end with the suffix.
   # suf_new must be a character string, dataframe must be a dataframe
   
-  namedf <- tibble::as_tibble(names(dataframe))  %>% 
-    mutate(new_names = ifelse(endsWith(value, suf_old), substr(value, 1,  (nchar(value)-nchar(suf_old))), value))
+  namedf <- tibble::as_tibble(names(dataframe))  |> 
+    dplyr::mutate(new_names = ifelse(endsWith(value, suf_old), 
+                                     substr(value, 1,  (nchar(value)-nchar(suf_old))), 
+                                     value))
   # new_names = paste0(value, suf_new))
   
-  names(dataframe) <- namedf %>% pull(new_names)
+  names(dataframe) <- namedf |> dplyr::pull(new_names)
   return(dataframe)
 }
